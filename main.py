8074e61a9fd66ffcca8fc0205fa64faa02ebad62
Drew White <d.white0002@gmail.com>
2022-11-04 09:57:00 -0700

    wrote out dict sayer function

--- main.py
+++ main.py
@@ -18,5 +18,17 @@ def list_sayer(list):  # create function called list_sayer() that takes list as 
 
 list_sayer(empty_list)
 
+# Example dictionaries
+dict_test = {"A": 1, "B": 2, "C": 3}
+dict_empty = {}
+# write function that takes dictionary as an argument
 
-def dict_sayer()
+
+def dict_sayer(dictionary):
+
+    for key, value in dictionary.items():
+      print("Key: {} Value: {}".format(key, value))
+      return (True)
+    if len(dictionary) == 0:
+      print("Dictionary empty"
+    return (False)
