456a8eaf88e3a4807f47eae64b8cd2d2ef2b594b
Drew White <d.white0002@gmail.com>
2022-11-04 12:06:26 -0700

    add comments to functions

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 # test lists
 things = ["guitar", "cup", "watch"]
 empty_list = []
+# write function called list_sayer() that takes list as an argument
 
 
-def list_sayer(list):  # write function called list_sayer() that takes list as an argument
-
-    if list == []:  # if list is empty return false list is empty
+def list_sayer(list):
+    # if list is empty return false list is empty
+    if list == []:
         print("The list is empty")
         return (False)
         # used for loop and enumerate to print list with name and index
@@ -18,7 +19,7 @@ def list_sayer(list):  # write function called list_sayer() that takes list as a
 
 list_sayer(empty_list)
 
-# Example dictionaries
+# test dictionaries
 dict_test = {"A": 1, "B": 2, "C": 3}
 dict_empty = {}
 # write function called dict_sayer that takes dictionary as an argument
