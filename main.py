753bd32ab495237ebab28cebc3809c004d52aa00
Drew White <d.white0002@gmail.com>
2022-11-04 09:02:49 -0700

    add function but only puts out one item from list WIP

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
+# test list
+things = ["guitar", "cup", "watch"]
 # create function called list_sayer() that takes list as an argument
+
+
+def list_sayer(list):
+    # if list is empty return false list is empty
+    if list == []:
+        print(False, "The list is empty")
+# if list has items return items with index and true
+    for index in range(len(list)):
+        print("Item: {} Index: {}".format(list[index], index))
+        return (True)
+
+
+list_sayer(things)
