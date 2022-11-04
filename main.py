9df55fb99334244eb93bf45136a9b4c0caffcdb3
Drew White <d.white0002@gmail.com>
2022-11-04 09:39:33 -0700

    completed function for list sayer with no errors

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
-# test list
+
+# test lists
 things = ["guitar", "cup", "watch"]
-# create function called list_sayer() that takes list as an argument
+empty_list = []
 
 
-def list_sayer(list):
-    # if list is empty return false list is empty
-    if list == []:
-        print(False, "The list is empty")
-# if list has items return items with index and true
-    for index in range(len(list)):
-        print("Item: {} Index: {}".format(list[index], index))
+def list_sayer(list):  # create function called list_sayer() that takes list as an argument
+
+    if list == []:  # if list is empty return false list is empty
+        print("The list is empty")
+        return (False)
+        # used for loop and enumerate to print list with name and index
+        for i, name in enumerate(list):
+            # if list has items print items with index and return true
+            print(f"Index: {i} Item: {name}")
         return (True)
 
 
-list_sayer(things)
+list_sayer(empty_list)
+
+
+def dict_sayer()
